056625adab6572ed62e9635c6452eec5cabe9d47
willitscale <james@n3tw0rk.co.uk>
2017-09-23 17:05:24 +0200

    Added tutorial 10

---
+++ tutorial-10/ERC223.sol
@@ -0,0 +1,6 @@
+pragma solidity ^0.4.0;
+
+interface ERC223 {
+    function transfer(address _to, uint _value, bytes _data) public returns (bool);
+    event Transfer(address indexed from, address indexed to, uint value, bytes indexed data);
+}
---
+++ tutorial-10/MyFirstToken.sol
@@ -0,0 +1,79 @@
+pragma solidity ^0.4.0;
+
+import "browser/Token.sol";
+import "browser/ERC20.sol";
+import "browser/ERC223.sol";
+import "browser/ERC223ReceivingContract.sol";
+
+contract MyFirstToken is Token("MFT", "My First Token", 18, 1000), ERC20, ERC223 {
+
+    function MyFirstToken() public {
+        _balanceOf[msg.sender] = _totalSupply;
+    }
+    
+    function totalSupply() public constant returns (uint) {
+        return _totalSupply;
+    }
+    
+    function balanceOf(address _addr) public constant returns (uint) {
+        return _balanceOf[_addr];
+    }
+
+    function transfer(address _to, uint _value) public returns (bool) {
+        if (_value > 0 && 
+            _value <= _balanceOf[msg.sender] &&
+            !isContract(_to)) {
+            _balanceOf[msg.sender] -= _value;
+            _balanceOf[_to] += _value;
+            Transfer(msg.sender, _to, _value);
+            return true;
+        }
+        return false;
+    }
+
+    function transfer(address _to, uint _value, bytes _data) public returns (bool) {
+        if (_value > 0 && 
+            _value <= _balanceOf[msg.sender] &&
+            isContract(_to)) {
+            _balanceOf[msg.sender] -= _value;
+            _balanceOf[_to] += _value;
+            ERC223ReceivingContract _contract = ERC223ReceivingContract(_to);
+                _contract.tokenFallback(msg.sender, _value, _data);
+            Transfer(msg.sender, _to, _value, _data);
+            return true;
+        }
+        return false;
+    }
+
+    function isContract(address _addr) returns (bool) {
+        uint codeSize;
+        assembly {
+            codeSize := extcodesize(_addr)
+        }
+        return codeSize > 0;
+    }
+
+    function transferFrom(address _from, address _to, uint _value) public returns (bool) {
+        if (_allowances[_from][msg.sender] > 0 &&
+            _value > 0 &&
+            _allowances[_from][msg.sender] >= _value &&
+            _balanceOf[_from] >= _value) {
+            _balanceOf[_from] -= _value;
+            _balanceOf[_to] += _value;
+            _allowances[_from][msg.sender] -= _value;
+            Transfer(_from, _to, _value);
+            return true;
+        }
+        return false;
+    }
+    
+    function approve(address _spender, uint _value) public returns (bool) {
+        _allowances[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+        return true;
+    }
+    
+    function allowance(address _owner, address _spender) public constant returns (uint) {
+        return _allowances[_owner][_spender];
+    }
+}
