4406d9d9b567bc1b2922c3048c7c3b7196365356
MBakhshi96 <34487501+MBakhshi96@users.noreply.github.com>
2018-07-24 14:05:22 +0430

    added some comments

--- tutorial-08/Debugging.sol
+++ tutorial-08/Debugging.sol
@@ -5,17 +5,20 @@ contract Debugging {
     uint[] private vars;
     
     function assignment() {
+        //stack Debugging
         uint myVal1 = 1;
         uint myVal2 = 2;
         assert(myVal1 == myVal2);
     }
     
     function memoryAlloc() {
+        //memory Debugging
         string memory myString = "test";
         assert(bytes(myString).length == 10);
     }
     
     function storageAlloc() {
+        //storage Debugging
         vars.push(2);
         vars.push(3);
         assert(vars.length == 4);
