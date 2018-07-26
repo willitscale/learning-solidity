4d64819cfba850de03febce5f905a02a64fc0ec1
MBakhshi96 <34487501+MBakhshi96@users.noreply.github.com>
2018-07-26 19:23:44 +0430

    added some useful comments

--- tutorial-15/ExternalContract.sol
+++ tutorial-15/ExternalContract.sol
@@ -1,11 +1,16 @@
 pragma solidity ^0.4.0;
 
+//link http://solidity.readthedocs.io/en/develop/contracts.html#visibility-and-getters
 contract ExternalContract {
+    //for calling externalCall use this.externalCall()
+    //input is passed to stack
     function externalCall(string x) external returns (uint) {
         return 123;
     }
     
+    //for calling publicCall use publicCall()
+    //input is passed to memory
     function publicCall(string x) public returns (uint) {
-        return 123;
+        return 123;     
     }
 }
