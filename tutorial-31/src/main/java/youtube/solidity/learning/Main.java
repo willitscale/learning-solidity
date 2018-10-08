46fd2ea3635210c3cacebad9215d5cbcc0c219b0
James Lockhart <james.lockhart@sykescottages.co.uk>
2018-10-08 01:39:30 +0100

    Tutorial 31
    Added tutorial 31

---
+++ tutorial-31/src/main/java/youtube/solidity/learning/Main.java
@@ -0,0 +1,21 @@
+package youtube.solidity.learning;
+
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.methods.response.Web3ClientVersion;
+import org.web3j.protocol.http.HttpService;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        Web3j web3j = Web3j.build(new HttpService());
+        Web3ClientVersion web3ClientVersion = null;
+        try {
+            web3ClientVersion = web3j.web3ClientVersion().send();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String web3ClientVersionString = web3ClientVersion.getWeb3ClientVersion();
+        System.out.println("Web3 client version: " + web3ClientVersionString);
+    }
+}
