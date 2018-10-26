df670a57d966f6df0120595b1c0472c7b39d4d2e
James <james@n3tw0rk.co.uk>
2018-10-26 02:43:07 +0100

    Resolved lint warnings for tutorial-04

--- tutorial-04/testLibrary.sol
+++ tutorial-04/testLibrary.sol
@@ -5,19 +5,19 @@ import "browser/library.sol";
 contract TestLibrary {
     using IntExtended for uint;
     
-    function testIncrement(uint _base) returns (uint) {
+    function testIncrement(uint _base) public pure returns (uint) {
         return IntExtended.increment(_base);
     }
     
-    function testDecrement(uint _base) returns (uint) {
+    function testDecrement(uint _base) public pure returns (uint) {
         return IntExtended.decrement(_base);
     }
     
-    function testIncrementByValue(uint _base, uint _value) returns (uint) {
+    function testIncrementByValue(uint _base, uint _value) public pure returns (uint) {
         return _base.incrementByValue(_value);
     }
     
-    function testDecrementByValue(uint _base, uint _value) returns (uint) {
+    function testDecrementByValue(uint _base, uint _value) public pure returns (uint) {
         return _base.decrementByValue(_value);
     }
 }
