1dca9c414d949cbf99f97f9ddd1dc73e2379f42c
James <james@n3tw0rk.co.uk>
2018-10-26 03:23:50 +0100

    Resolved lint warnings for tutorial-09

--- tutorial-09/MyToken.sol
+++ tutorial-09/MyToken.sol
@@ -11,19 +11,19 @@ contract MyFirstToken is ERC20 {
     mapping (address => uint) private __balanceOf;
     mapping (address => mapping (address => uint)) private __allowances;
     
-    function MyFirstToken() {
+    constructor() public {
             __balanceOf[msg.sender] = __totalSupply;
     }
     
-    function totalSupply() constant returns (uint _totalSupply) {
+    function totalSupply() public constant returns (uint _totalSupply) {
         _totalSupply = __totalSupply;
     }
     
-    function balanceOf(address _addr) constant returns (uint balance) {
+    function balanceOf(address _addr) public constant returns (uint balance) {
         return __balanceOf[_addr];
     }
     
-    function transfer(address _to, uint _value) returns (bool success) {
+    function transfer(address _to, uint _value) public returns (bool success) {
         if (_value > 0 && _value <= balanceOf(msg.sender)) {
             __balanceOf[msg.sender] -= _value;
             __balanceOf[_to] += _value;
@@ -32,7 +32,7 @@ contract MyFirstToken is ERC20 {
         return false;
     }
     
-    function transferFrom(address _from, address _to, uint _value) returns (bool success) {
+    function transferFrom(address _from, address _to, uint _value) public returns (bool success) {
         if (__allowances[_from][msg.sender] > 0 &&
             _value > 0 &&
             __allowances[_from][msg.sender] >= _value && 
@@ -46,12 +46,12 @@ contract MyFirstToken is ERC20 {
         return false;
     }
     
-    function approve(address _spender, uint _value) returns (bool success) {
+    function approve(address _spender, uint _value) public returns (bool success) {
         __allowances[msg.sender][_spender] = _value;
         return true;
     }
     
-    function allowance(address _owner, address _spender) constant returns (uint remaining) {
+    function allowance(address _owner, address _spender) public constant returns (uint remaining) {
         return __allowances[_owner][_spender];
     }
 }
