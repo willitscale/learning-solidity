ab35274531b58dc5c32efeb1e2c0edad95064405
James Lockhart <james.lockhart@sykescottages.co.uk>
2019-10-24 19:45:51 +0100

    Made tutorial 1 work for the latest version of Solidity

--- tutorial-01/myfirstcontract.sol
+++ tutorial-01/myfirstcontract.sol
@@ -1,14 +1,14 @@
-pragma solidity ^0.4.0;
+pragma solidity ^0.5.0;
 
 contract MyFirstContract {
     string private name;
     uint private age;
     
-    function setName(string newName) public {
+    function setName(string memory newName) public {
         name = newName;
     }
     
-    function getName() public view returns (string) {
+    function getName() public view returns (string memory) {
         return name;
     }
     
