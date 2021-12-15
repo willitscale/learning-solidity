838f5b9250f86ea165b556c012524af19d613d0d
"Jordan Loudis" <"16jloudis@gmail.com">
2021-12-14 19:31:42 -0500

    Fix Tutorials 1 & 2 to work with the latest solidity version - ^0.8.7

--- tutorial-02/myfirstcontract.sol
+++ tutorial-02/myfirstcontract.sol
@@ -1,58 +1,85 @@
-pragma solidity ^0.5.0;
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
 
 interface Regulator {
     function checkValue(uint amount) external returns (bool);
     function loan() external returns (bool);
 }
 
-contract Bank is Regulator {
-    uint private value;
-    
-    constructor(uint amount) public {
-        value = amount;
+contract Bank is Regulator{
+
+    // The internal modifer means that the function 
+    // can only be called within the contract itself and any derived contracts.
+    // ex. uint internal myInternalValue
+
+    uint private bankValue;
+
+    constructor(uint amount) {
+
+        bankValue = amount;
     }
-    
+
     function deposit(uint amount) public {
-        value += amount;
+        bankValue += amount;
     }
-    
-    function withdraw(uint amount) public {
-        if (checkValue(amount)) {
-            value -= amount;
+
+    function withdrawel(uint amount) public {
+
+        if (checkValue(amount)){
+             bankValue -= amount;
+
         }
     }
-    
+
     function balance() public view returns (uint) {
-        return value;
+
+        return bankValue;
     }
-    
-    function checkValue(uint amount) public returns (bool) {
-        // Classic mistake in the tutorial value should be above the amount
-        return value >= amount;
+
+    // Override
+
+    function checkValue(uint amount) public override view returns (bool) {
+
+        return bankValue > amount;
+
     }
-    
-    function loan() public returns (bool) {
-        return value > 0;
+
+    function loan() public override view returns (bool) {
+
+        return bankValue > 0;
+
     }
+
 }
 
-contract MyFirstContract is Bank(10) {
+contract myfirstcontract is Bank(10) {
+
+    // Opposite of Java - private String firstName;
     string private name;
     uint private age;
-    
+
+
     function setName(string memory newName) public {
+        
         name = newName;
+
     }
-    
+
     function getName() public view returns (string memory) {
         return name;
+
     }
-    
+
     function setAge(uint newAge) public {
         age = newAge;
     }
-    
+
     function getAge() public view returns (uint) {
         return age;
+
     }
+
+    
+
 }
